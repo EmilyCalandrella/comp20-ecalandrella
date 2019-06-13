3e95df5d1fd0835d8c651086d2196afd749e571e
EmilyC <emily.calandrella@tufts.edu>
2019-06-12 21:01:45 -0400

    attempting to get cars to show up

---
+++ notuber/map.js
@@ -0,0 +1,23 @@
+      var map;
+      function initMap() {
+        map = new google.maps.Map(document.getElementById('map'), {
+          center: {lat: 42.352271, lng: -71.05524200000001},
+          zoom: 14
+        });
+      }
+
+
+function addCars {
+	var car = 'car.png';
+	var location1 = new google.maps.LatLng(42.3453, -71.0464);
+
+
+	var car1 = new google.maps.Marker ({
+		map: map,
+		position: location1,
+		title: "mXfkjrFw",
+		icon: car
+	})
+
+	car1.setMap(map);
+}
