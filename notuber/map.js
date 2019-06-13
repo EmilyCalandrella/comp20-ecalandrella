2c0312e4c2565183d4d2865151efc131efbb72c5
EmilyC <emily.calandrella@tufts.edu>
2019-06-12 21:21:08 -0400

    put map initialization back in html

--- notuber/map.js
+++ notuber/map.js
@@ -1,10 +1,10 @@
-      var map;
+   /*   var map;
       function initMap() {
         map = new google.maps.Map(document.getElementById('map'), {
           center: {lat: 42.352271, lng: -71.05524200000001},
           zoom: 14
         });
-      }
+      }*/
 
 
 function addCars {
