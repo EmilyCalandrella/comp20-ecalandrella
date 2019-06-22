d50cc72b3c0d27d333c032603e3e8fb4b529de78
EmilyC <emily.calandrella@tufts.edu>
2019-06-21 20:28:33 -0400

    add polyline

--- notuber/map.js
+++ notuber/map.js
@@ -7,81 +7,111 @@ var carIcon = 'car.png';
 var carCount = 0;
 
 function initMap() {
-  map = new google.maps.Map(document.getElementById('map'), {
-    center: {lat: 42.352271, lng: -71.05524200000001},
-    zoom: 12
-  })
+
+  	map = new google.maps.Map(document.getElementById('map'), {
+    	center: {lat: 42.352271, lng: -71.05524200000001},
+    	zoom: 11
+  	})
 
   
-  if (navigator.geolocation) {
-    navigator.geolocation.getCurrentPosition(function(position) {
-    	var myLat = position.coords.latitude;
-    	var myLng = position.coords.longitude;
-      var pos = {
-        lat: myLat,
-        lng: myLng
-      };
+	 if (navigator.geolocation) {
 
-      addMarker(pos);
-      addInfoWindow(pos);
-      var from = new google.maps.LatLng(pos);
+	    navigator.geolocation.getCurrentPosition(function(position) {
+	    	var myLat = position.coords.latitude;
+	    	var myLng = position.coords.longitude;
+	      	var pos = {
+	        	lat: myLat,
+	        	lng: myLng
+	      	};	
 
+	      	addMarker(pos);
+	      	var from = new google.maps.LatLng(pos);
 
-	  	var xhr = new XMLHttpRequest();
-		xhr.open('POST', 'https://hans-moleman.herokuapp.com/rides', true);
-		xhr.setRequestHeader("Content-type", "application/x-www-form-urlencoded");
-		xhr.onreadystatechange = function() {
-		    if(xhr.readyState == 4 && xhr.status == 200) {
-		    	jsonData = xhr.responseText;
-		    	outputs = JSON.parse(jsonData);
-		    	var cars = {};
-		    	var carMark;
-		    	var distances = {};
-		    	var to;
-				for (var i = 0; i < outputs.length; i++) {
-					carCount++;
-					cars[i] = outputs[i];
-					//to = new google.maps.LatLng(carLat, carLng);
-					to = new google.maps.LatLng(cars[i].lat, cars[i].lng)
-					carMark = new google.maps.Marker ({
-						map: map,
-						position: to, //{lat: car.lat, lng: car.lng},
-						title: cars[i].username,
-						icon: carIcon
-					})
-					distances[i] = google.maps.geometry.spherical.computeDistanceBetween(from, to);
-				}
-				var shortest = distances[0];
-				for (var j = 0; j < carCount; j++) {
-					if (distances[j] < shortest) {
-						shortest = distances[j];
+		  	var xhr = new XMLHttpRequest();
+			xhr.open('POST', 'https://hans-moleman.herokuapp.com/rides', true);
+			xhr.setRequestHeader("Content-type", "application/x-www-form-urlencoded");
+			xhr.onreadystatechange = function() {
+			    if(xhr.readyState == 4 && xhr.status == 200) {
+			    	jsonData = xhr.responseText;
+			    	outputs = JSON.parse(jsonData);
+			    	var cars = {};
+			    	var carMark;
+			    	var distances = {};
+			    	//var to;
+			    	var locations = {};
+			    	//var shortest;
+
+					for (var i = 0; i < outputs.length; i++) {
+						carCount++;
+						cars[i] = outputs[i];
+						//to = new google.maps.LatLng(cars[i].lat, cars[i].lng)
+						locations[i] = new google.maps.LatLng(cars[i].lat, cars[i].lng)
+						carMark = new google.maps.Marker ({
+							map: map,
+							position: locations[i],
+							title: cars[i].username,
+							icon: carIcon
+						})
+						distances[i] = google.maps.geometry.spherical.computeDistanceBetween(from, locations[i]);
 					}
-				}
+
+					var shortest = distances[0];
+					var index = 0;
+
+					for (var j = 0; j < carCount; j++) {
+						if (distances[j] < shortest) {
+							shortest = distances[j];
+							index = j;
+						}
+					}
+
+					var miles = shortest * 0.000621371;
+					var rounded = Math.round(miles * 100) / 100;
+					marker.addListener("click", function() {
+	      				infoWindow = new google.maps.InfoWindow;
+	       				infoWindow.setPosition(pos);
+	        			infoWindow.setContent('Closest vehicle: ' + cars[index].username + ' (' + rounded + ' miles away)');
+	        			infoWindow.open(map);
+	      			})
+
+					// should try saving locations in an array in the first place
+					//var location = new google.maps.LatLng(cars[index].lat, cars[index].lng);
+					//console.log(locations[index]);
+					var path = [pos, locations[index]];
+					var line = new google.maps.Polyline({
+						path: path,
+						geodesic: true,
+						strokeColor: '#FF0000',
+						strokeWeight: 2
+					});
+
+					line.setMap(map);	
+			    } // end of if ready
+
+			} // end of onreadystatechange
+
+			// still inside get current position function
+
+		    var params = "username=6ST1sfMe&lat=" + myLat + "&lng=" + myLng;
+		    xhr.send(params);
 
 
-				
-		    }
-
-
-		}
-	    var params = "username=6ST1sfMe&lat=" + myLat + "&lng=" + myLng;
-	    xhr.send(params);
-
-
-    }, function() {
-      handleLocationError(true, infoWindow, map.getCenter());
-    });
-  } else {
-    // Browser doesn't support Geolocation
-    handleLocationError(false, infoWindow, map.getCenter());
-  }
+	    }, 	function() {
+	      handleLocationError(true, infoWindow, map.getCenter());
+	    }); // end of second get current position function, end of section
 
 
 
+	  } // end of if part 
+	  else {
+	    // Browser doesn't support Geolocation
+	    handleLocationError(false, infoWindow, map.getCenter());
+	  } // end of if else (checking for geolocation)
 
 
+} // end of init map
 
-}
+
 
 function addMarker(pos) {
         marker = new google.maps.Marker ({
@@ -91,14 +121,14 @@ function addMarker(pos) {
       })	
 }
 
-function addInfoWindow(pos) {
+/*function addInfoWindow(pos) {
       marker.addListener("click", function() {
       	infoWindow = new google.maps.InfoWindow;
         infoWindow.setPosition(pos);
         infoWindow.setContent('The closest vehicle is.....');
         infoWindow.open(map);
       })
-}
+}*/
 
 
 
